514fba9e6419ab55d48bb19bc5be8bef1cfbe362
davham <david.hamilton5@gmail.com>
2023-03-08 17:03:37 -0500

    Password generator project
    A random password generator app that uses html/CSS and javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let pass1El = document.getElementById("pass1")
+let pass2El = document.getElementById("pass2")
+
+
+function generate(){
+
+  pass1El.textContent = " "
+  pass2El.textContent = " "
+  
+  for( let i = 0; i < 15; i++ ){
+    let rand1 = Math.floor(Math.random() * characters.length)
+    let rand2 = Math.floor(Math.random() * characters.length)
+
+    pass1El.textContent += characters[rand1]
+    pass2El.textContent += characters[rand2]
+  }
+   
+}
